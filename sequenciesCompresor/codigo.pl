6e00bdff5cd823f8a459e5549adcf94bee58feb6
Guillermo Lopez <guilogar42@gmail.com>
2021-05-09 21:36:03 +0200

    hasta la fase C

--- sequenciesCompresor/codigo.pl
+++ sequenciesCompresor/codigo.pl
@@ -7,7 +7,9 @@ comprimir(Inicial,Comprimida) :-
     limpia_memo,
     compresion_recursiva(Inicial,Comprimida).
 limpia_memo.
-compresion_recursiva(Inicial ,Inicial).
+
+compresion_recursiva(Inicial,Comprimida) :-
+    mejor_compresion(Inicial,Comprimida).
 
 
 partir(Todo, Parte1, Parte2) :-
@@ -26,13 +28,49 @@ parentesis(Parte,Num,ParteNum) :-
         append(Parte,[Num],ParteNum)
     ).
 
-
 se_repite(Cs,Parte,Num0,Num) :-
     n_repeticiones(Cs,Parte,N),
     Num is Num0 + N.
 
 
+%FASE A
 
+repeticion(Todo,Resultado):-
+    %de esta forma prueba con todas las partes posibles
+    partir(Todo,Parte1,_Parte2),
+    se_repite(Todo,Parte1,0,N),
+    compresion_recursiva(Parte1,Comprimida),
+    parentesis(Comprimida,N,Resultado).
+
+
+%FASE B
+
+compresion(Inicial,Comprimida) :-
+    repeticion(Inicial,Comprimida).
+compresion(Inicial,Comprimida) :-
+    division(Inicial,Comprimida).
+
+division(Todo,Resultado):-
+    partir(Todo,Parte1,Parte2),
+    compresion_recursiva(Parte1,Comprimida1),
+    compresion_recursiva(Parte2,Comprimida2),
+    append(Comprimida1,Comprimida2,Resultado).
+
+
+%FASE C
+
+mejor_compresion(Inicial,Comprimida) :-
+    findall(X,compresion(Inicial,X),[Lista1|Resto]),
+    length(Inicial,LongMax),
+    shortest_list([Lista1|Resto],Lista1,ResultadoTemporal,LongMax),
+    length(ResultadoTemporal,LTemp),
+    ( LTemp >= LongMax ->
+        Comprimida = Inicial
+    ;
+        Comprimida = ResultadoTemporal
+    ),
+    !. %No se necesita que siga buscando
+mejor_compresion(Inicial,Inicial).    
 
 %Programas auxiliares
 
@@ -47,3 +85,15 @@ n_repeticiones(Todo,Parte,N) :-
     append(ListaAux,Parte,Todo),
     n_repeticiones(ListaAux,Parte,Np),
     N is Np + 1.
+
+shortest_list([],Resultado,Resultado,_N).
+shortest_list([Lista|Resto],Shortest,Resultado,N) :-
+    length(Lista,Length),
+    N =< Length,
+    shortest_list(Resto,Shortest,Resultado,N).
+shortest_list([Lista|Resto],_Shortest,Resultado,N) :-
+    length(Lista,Length),
+    N > Length,
+    shortest_list(Resto,Lista,Resultado,Length).
+
+    
